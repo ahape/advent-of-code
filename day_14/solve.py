c0ba679c18933f4cd756559dbde61962fcc4d69a
Alan Hape <ahape@brightmetrics.com>
2022-12-14 23:57:01 -0800

    day14: p1&2

--- day_14/solve.py
+++ day_14/solve.py
@@ -1,6 +1,13 @@
 import sys
 
-class AbyssReachedError(Exception):
+PART_TWO = len(sys.argv) > 1
+
+drop_x = 500
+
+if PART_TWO:
+  max_width = 328 # Good number to crop to. See output.txt
+
+class AbyssReached(Exception):
   pass
 
 def parse_path(text):
@@ -19,6 +26,7 @@ def get_grid_edges(points):
   )
 
 def create_grid(path):
+  global offset
   xmin = xmax = ymax = None
   ymin = 0
   for point in path:
@@ -29,36 +37,42 @@ def create_grid(path):
       xmax = _xmax
     if ymax is None or _ymax > ymax:
       ymax = _ymax
+  y_span = ymax - ymin + (3 if PART_TWO else 1)
+  x_span = max_width if PART_TWO else (xmax - xmin) + 3
   grid = []
-  for _ in range((ymax - ymin) + 1):
+  for _ in range(y_span):
     row = []
+    # Part one:
     # '3' instead of '1' to make room for the edges that drop into the abyss.
     # 1 for the left edge, 1 for the right
-    for _ in range((xmax - xmin) + 3):
+    for _ in range(x_span):
       row.append(".")
     grid.append(row)
-  
-  return grid, xmin - 1
+
+  offset = -(drop_x-(max_width//2)) if PART_TWO else -(xmin - 1)
+  return grid
 
 def print_grid(grid):
   canvas = ""
   i = 0
   for row in grid:
-    canvas += f"{i} {''.join(row)}\n"
+    canvas += f"{i:04d} {''.join(row)}\n"
     i += 1
   print(canvas)
 
-def drop_sand_loop(grid, xoffset):
-  grains = 0
+def drop_sand_loop(grid):
+  x, y, grains = 0, 0, 0
   try:
-    while True:
-      [x, y], hit = drop_sand(grid, 500-xoffset)
+    while not (y == 0 and x == drop_x + offset):
+      [x, y], hit = drop_sand(grid, drop_x + offset)
       if hit == "o":
         x, y = settle_sand(grid, [x, y])
       grid[y][x] = "o"
       grains += 1
-  except AbyssReachedError:
-    return grains
+  except AbyssReached:
+    pass
+
+  return grains
 
 def drop_sand(grid, x=0, y=0):
   for row in grid[y:]:
@@ -80,7 +94,7 @@ def settle_sand_diagonally(grid, point):
     if can_try_left:
       [x, y], hit = drop_sand(grid, x + left, y + 1)
       if hit == "~":
-        raise AbyssReachedError
+        raise AbyssReached
       point = [x, y]
       continue
 
@@ -90,7 +104,8 @@ def settle_sand_diagonally(grid, point):
     if can_try_right:
       [x, y], hit = drop_sand(grid, x + right, y + 1)
       if hit == "~":
-        raise AbyssReachedError
+        break
+        #raise AbyssReached
       point = [x, y]
       continue
 
@@ -104,7 +119,7 @@ def settle_sand(grid, point):
     return new_spot
   return point
 
-def draw_rocks(path, grid, xoffset):
+def draw_rocks(path, grid):
   last = None
   for point in path:
     x, y = point
@@ -112,26 +127,27 @@ def draw_rocks(path, grid, xoffset):
       lastx, lasty = last
       if x == lastx: # vert
         for _y in range(min(y, lasty), max(y, lasty)):
-          grid[_y][x - xoffset] = "#"
+          grid[_y][x + offset] = "#"
       else:
         for _x in range(min(x, lastx), max(x, lastx)):
-          grid[y][_x - xoffset] = "#"
-    grid[y][x - xoffset] = "#"
+          grid[y][_x + offset] = "#"
+    grid[y][x + offset] = "#"
     last = point[:]
+
+  if PART_TWO:
+    grid[-1] = ["#" for _ in range(max_width)]
 
 with open("input.txt") as file:
   paths = []
   for line in file.readlines():
     paths.append(parse_path(line.strip()))
 
-  grid, xmin = create_grid(paths)
+  grid = create_grid(paths)
 
   for point in paths:
-    draw_rocks(point, grid, xmin)
+    draw_rocks(point, grid)
 
-  #grains = drop_sand_loop(grid, xmin, int(sys.argv[1]))
-
-  grains = drop_sand_loop(grid, xmin)
+  grains = drop_sand_loop(grid)
 
   print_grid(grid)
   print("Grains landed", grains)
