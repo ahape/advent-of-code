ca219d6eb9c435a5959e598860f3aa6526412096
Alan Hape <ahape@brightmetrics.com>
2022-12-21 00:12:52 -0800

    wip idk d20,p1

--- day_20/solve.py
+++ day_20/solve.py
@@ -12,39 +12,38 @@ def parse_line(line):
   return Entry(line)
 
 def mix(coords, entry, boundary):
-  if entry.is_zero:
-    return coords
-  i = abs(coords.index(entry) + entry.value) % boundary
+  current_i = coords.index(entry)
+  i = abs(current_i + entry.value) % boundary
+
   if entry.value < 0:
     i = boundary - i
-  coords.remove(entry)
-  coords.insert(i, entry)
+
+  if current_i != i:
+    if current_i < i:
+      coords.remove(entry)
+
+    coords.insert(i, Entry(entry.value))
+
+    if current_i > i:
+      coords.remove(entry)
+
   return coords
 
-with open("before.txt", "r") as file:
+with open("example.txt", "r") as file:
   coords = []
   for line in file.readlines():
     coords.append(parse_line(line.strip()))
 
-  # Need to know where something traveled
-
-  i, leng, mixed = 0, len(coords), coords[:]
+  leng, mixed = len(coords), coords[:]
   for entry in coords:
     mixed = mix(mixed, entry, leng-1)
-    break
     #print(entry, mixed)
-    i += 1
 
   nums = [x.value for x in mixed]
-  print(len(nums), min(nums), max(nums))
   to_add = []
-  zero_i = [i for i, e in enumerate(mixed) if e.is_zero][0]
+  zero_i = [i for i, e in enumerate(mixed) if e.value == 0][0]
 
-  for i in range(max(grove_indexes) + 1):
-    if i in grove_indexes:
-      to_add.append(nums[(zero_i + i) % leng])
+  for i in grove_indexes:
+    to_add.append(nums[(zero_i + i) % leng])
 
-  with open("after.txt", "w+") as output:
-    output.write("\n".join([str(x) for x in nums]))
-  #assert(tuple(mixed) == (1, 2, -3, 4, 0, 3, -2))
-  #print(sum(to_add))
+  print(to_add, sum(to_add))
