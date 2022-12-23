5a13515d7d6868f6fedf7e27ffc1ba6e361fed84
Alan Hape <ahape@brightmetrics.com>
2022-12-23 11:35:06 -0800

    d21,p1 wip prints accurate example-ish

--- day_21/solve.py
+++ day_21/solve.py
@@ -1,31 +1,47 @@
 job_queue = {}
 
+def add_to_queue(monkey, job):
+  if monkey not in job_queue:
+    job_queue[monkey] = []
+  job_queue[monkey].append(job)
+  job.pending.append(monkey)
+
+def resolve(monkey, value):
+  for jobs in job_queue[monkey]:
+    for i, part in enumerate(job.desc[:]):
+      job.desc[i] = value if part == monkey else part
+    remove_from_queue(monkey, job)
+
+def remove_from_queue(monkey, job):
+  job_queue[monkey].remove(job)
+  job.pending.append(monkey)
+
 class Job():
-  def __init__(self, job):
-    self.job = job
+  def __init__(self, desc):
+    self.desc = desc
     self.pending = []
     self.value = None
-    a, a_is_int = try_parse_int(job[0])
-    if len(job) == 1:
+    a, a_is_int = try_parse_int(desc[0])
+    if len(desc) == 1:
       if a_is_int:
         self.value = a
       else:
         raise "Unexpected unary"
     if self.value == None:
       if not a_is_int:
-        self.pending.append(a)
-      _, operator, b = job
+        add_to_queue(a, self)
+      _, operator, b = desc
       b, b_is_int = try_parse_int(b)
       if not b_is_int:
-        self.pending.append(b)
+        add_to_queue(b, self)
 
   def __repr__(self):
-    return f"{self.job} [{'pending' if len(self.pending) else 'resolved'}]"
+    return f"{self.desc} [{'pending' if len(self.pending) else 'resolved'}]"
 
 def parse_line(line):
-  monkey, job = line.split(":")
-  job = job.strip()
-  return (monkey, Job(job.split(" ")))
+  monkey, desc = line.split(":")
+  desc = desc.strip().split(" ")
+  return (monkey, Job(desc))
 
 def try_parse_int(s):
   try:
