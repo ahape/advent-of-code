51c7da6c6d89c865b8aa9906afd78949330f1c2c
Alan Hape <ahape@brightmetrics.com>
2022-12-22 22:57:06 -0800

    working LL model

--- day_20/solve.py
+++ day_20/solve.py
@@ -1,41 +1,50 @@
 grove_indexes = [1000, 2000, 3000]
 
-class Link():
+class Node():
   def __init__(self, val):
     self.value = int(val)
     self.prev = None
     self.next = None
+
+  def detach(self):
+    self.next.prev = self.prev
+    self.prev.next = self.next
+
+  def attach(self, node, before):
+    if before:
+      self.next = node
+      self.prev = node.prev
+      node.prev.next = node.prev = self
+    else:
+      self.prev = node
+      self.next = node.next
+      node.next.prev = node.next = self
+
+  def get_node(self, distance):
+    node = self
+    move_left = distance < 0
+    while distance:
+      node = node.prev if move_left else node.next
+      if node == self:
+        continue
+      distance += (1 if move_left else -1)
+    return node
+
   def __repr__(self):
     return str(self.value)
 
-def mix(link):
-  left = link.value < 0
-  last = link
-  old_prev = link.prev
-  old_next = link.next
+def mix(node):
+  if node.value:
+    node.detach()
+    other = node.get_node(node.value)
+    node.attach(other, node.value < 0)
 
-  if not link.value:
-    return
+def zero_node(node):
+  while node.value != 0:
+    node = node.next
+  return node
 
-  for _ in range(abs(link.value)):
-    last = last.prev if left else last.next
-
-  if left:
-    link.next = last
-    link.prev = last.prev
-    last.prev.next = last.prev = link
-  else:
-    link.prev = last
-    link.next = last.next
-    last.next.prev = last.next = link
-
-  old_prev.next = old_next
-  old_next.prev = old_prev
-
-def zero_link(coords):
-  return [e for i, e in enumerate(coords) if e.value == 0][0]
-
-def grove_coords(coords, zero):
+def grove_coords(nodes, zero):
   to_add = []
   cur = zero
   for i in range(max(grove_indexes) + 1):
@@ -44,35 +53,35 @@ def grove_coords(coords, zero):
     cur = cur.next
   return to_add
 
-def print_coords(coords, link):
+def print_coords(nodes, node):
   actual = []
-  for i in range(len(coords)):
-    actual.append(link.value)
-    link = link.next
+  for i in range(len(nodes)):
+    actual.append(node.value)
+    node = node.next
   print("What's there", actual)
 
-def create_linked(file):
-  coords = []
+def create_nodes(file):
+  nodes = []
   prev = cur = None
   for line in file.readlines():
-    cur = Link(line.strip())
+    cur = Node(line.strip())
     if prev:
       cur.prev = prev
       prev.next = cur
-    coords.append(cur)
+    nodes.append(cur)
     prev = cur
-  coords[0].prev = cur
+  nodes[0].prev = cur
   cur.next = prev
-  return coords
+  return nodes
 
-with open("example.txt", "r") as file:
-  coords = create_linked(file)
+with open("input.txt", "r") as file:
+  nodes = create_nodes(file)
 
-  for entry in coords:
+  for entry in nodes:
     mix(entry)
-    #print_coords(coords, entry)
+    #print_coords(nodes, entry)
 
-  to_add = grove_coords(coords, zero_link(coords))
+  to_add = grove_coords(nodes, zero_node(nodes[0]))
   summed = sum(to_add)
-  assert 3 == summed, (to_add, summed)
-  #assert 8764 == summed, (to_add, summed)
+  #assert 3 == summed, (to_add, summed)
+  assert 8764 == summed, (to_add, summed)
