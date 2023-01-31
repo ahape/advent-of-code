8050c93bf276417af9f91fff93a65310d265b39a
Alan Hape <ahape@brightmetrics.com>
2023-01-30 21:58:39 -0800

    add render fn...yay

--- day_24/solve.py
+++ day_24/solve.py
@@ -3,11 +3,26 @@ WALL, BLIZZARD_TYPE, EMPTY = "#", "<v>^", "."
 BLIZZ_N, BLIZZ_S, BLIZZ_E, BLIZZ_W = BLIZZARD_TYPE
 WALLS, BLIZZARDS = set(), []
 ENTRY, EXIT = None, None
+DIMS = { "H": 0, "W": 0 }
+
+def draw_frame():
+  canvas = []
+  for y in range(DIMS["H"]):
+    canvas.append(["."] * DIMS["W"])
+  for x, y in WALLS:
+    canvas[y][x] = WALL
+  for c, x, y in BLIZZARDS:
+    canvas[y][x] = c
+  print("\n".join(map("".join, canvas)))
 
 def load_data():
   global ENTRY, EXIT
   with open(FILE_NAME, "r", encoding="utf-8") as file_in:
-    for y, line in enumerate(file_in.readlines()):
+    lines = file_in.readlines()
+    DIMS["H"] = len(lines)
+    for y, line in enumerate(lines):
+      if DIMS["W"] == 0:
+        DIMS["W"] = len(line)-1
       for x, c in enumerate(line):
         if c == EMPTY:
           if ENTRY is None:
@@ -21,10 +36,7 @@ def load_data():
 
 def main():
   load_data()
+  draw_frame()
 
 if __name__ == "__main__":
   main()
-  print("Blizzards", BLIZZARDS)
-  print("Walls", WALLS)
-  print("Entry", ENTRY)
-  print("Exit", EXIT)
