8910d6f93e9496f571944852bd964eac7b75b8db
Alan Hape <ahape@brightmetrics.com>
2023-12-01 18:31:29 -0800

    day_01 Completed

--- 2023/day_01/solve.py
+++ 2023/day_01/solve.py
@@ -1,50 +1,46 @@
-import re
-
-num_extractor_rx = re.compile(r"(\d|one|two|three|four|five|six|seven|eight|nine)")
 num_map = {
-  "one": 1,
-  "two": 2,
-  "three": 3,
-  "four": 4,
-  "five": 5,
-  "six": 6,
-  "seven": 7,
-  "eight": 8,
-  "nine": 9,
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5",
+  "six": "6",
+  "seven": "7",
+  "eight": "8",
+  "nine": "9",
 }
 
 def solve(data):
-  total = 0
-  for row in data:
-    num = extract_two_digit_num(row)
-    total += num
-  return total
+  return sum(extract_two_digit_num(row) for row in data)
 
 def extract_two_digit_num(row):
-  digits = []
-  for m in num_extractor_rx.finditer(row):
-    num = m[0]
-    if num.isdigit():
-      digits.append(num)
-    else:
-      digits.append(num_map[num])
-  res = f"{digits[0]}{digits[-1]}"
-  print(row, "-->", int(res))
+  first_num = last_num = first_index = last_index = None
+  for word, digit in num_map.items():
+    for num in [word, digit]:
+      if num in row:
+        s, e = row.index(num), row.rindex(num)
+        if first_index is None or s < first_index:
+          first_index = s
+          first_num = num if num.isdigit() else num_map[num] 
+        if last_index is None or e > last_index:
+          last_index = e
+          last_num = num if num.isdigit() else num_map[num] 
+  res = first_num + last_num
+  #print(row, "-->", res)
   return int(res)
 
-def main():
-  data = []
-  #with open("example-1.txt") as f:
-  #with open("example-2.txt") as f:
-  with open("example-3.txt") as f:
-  #with open("input.txt") as f:
-    for row in f.readlines():
-      row = row.strip()
-      if row:
-        data.append(row)
+def read_data(file):
+  with open(file) as f:
+    text = f.read().strip()
+    return text.split("\n")
 
+def main():
+  #data = read_data("example-1.txt")
+  #data = read_data("example-2.txt")
+  #data = read_data("example-3.txt")
+  data = read_data("input.txt")
   answer = solve(data)
-  print(answer)
+  print("The Answer:", answer)
 
 if __name__ == "__main__":
   main()
